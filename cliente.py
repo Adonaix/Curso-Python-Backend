91d665ae0771b764cdc728afcce8c28e6bd4c922
Abdul Adonai Galindo Preciado <agalindopreciado@gmail.com>
2024-08-28 18:34:04 -0700

    Se anexá los procedimientos de put favorito y item por petición

--- cliente.py
+++ cliente.py
@@ -6,5 +6,18 @@ def get_usuario():
     response = requests.get(URL_BASE + "/usuario/2")
     print("Resultado:", response.json())
 
+def put_favorito():
+    respuesta = requests.put(URL_BASE + "/usuario/favorito",
+                              params={"nombre": "Bistec",
+                                  "ultima_fecha": "21 de agosto de 2024",
+                                  "id": 1})
+    print("Respuesta de favorito: ", respuesta.json())
+
+def get_item():
+    response = requests.get(URL_BASE + "/item", params={"id":"ajsdh"})
+    print("Resultado:", response.json())                               
+
 if __name__ == "__main__":
-    get_usuario()
+    #get_usuario()
+    #put_favorito()
+    get_item()
--- main.py
+++ main.py
@@ -12,4 +12,21 @@ def get_usuario(id:int):
     if not usuario:
         return "No se encontró el usuario."
     return usuario
-    
+
+
+@app.put("/usuario/favorito")
+def put_favorito(nombre: str, ultima_fecha: str, id: int):
+    usuario = usuarios.get(id)
+    if not usuario:
+        return "No se encontró el usuario."
+    usuario['favoritos'].append({
+        "nombre": nombre, "ultima-fecha": ultima_fecha,
+    })
+    return usuario
+
+@app.get("/item")
+def getItem(id: str):
+    item = items.get(id)
+    if not item:
+        return "No se encontró el usuario."
+    return item
